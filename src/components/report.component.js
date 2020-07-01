279720b356cd06b4b4db10c3152540d734724a75
Dananjaya Polwattage <nuwan_windows7@live.com>
2020-07-01 20:30:02 +0530

    Report Interface
    Report Interface  Completed, Backend report generation under development. :airplane:

--- src/components/report.component.js
+++ src/components/report.component.js
@@ -1,12 +1,107 @@
 import React , {Component} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import { render } from '@testing-library/react';
 
 export default class reportscomp extends Component{
+
+    constructor(props)
+    {
+        super(props);
+
+        this.onSubmitInbound = this.onSubmitInbound.bind(this);
+        this.onSubmitOutbound = this.onSubmitOutbound.bind(this);
+
+        this.state = {
+            start_date : new Date(),
+            end_date : new Date(),
+            generate_outbound : false,
+            generate_inbound : false
+        }
+    }
+
+    onChangeStartDate = start_date => this.setState({ start_date })
+    onChangeEndDate = end_date => this.setState({ end_date })
+
+    onSubmitInbound(e)
+    {
+        e.preventDefault();
+
+        console.log(`Report Strat Date : ${this.state.start_date}`);
+        console.log(`Report End Date : ${this.state.end_date}`);
+    }
+
+    onSubmitOutbound(e)
+    {
+        e.preventDefault();
+
+        console.log(`Report Strat Date : ${this.state.start_date}`);
+        console.log(`Report End Date : ${this.state.end_date}`);
+    }
+
     render()
     {
         return(
-            <div>
-                <p>Welcome to reports</p>
+            <div className="container">
+                <h2>Report Generator</h2>
+                <table cellPadding="20" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div className="form-group">
+                                    <label>Start Date :</label>
+                                </div>
+                            </td>
+
+                            <td>
+                                <div className="form-group">
+                                    <label>End Date :</label>
+                                </div>
+                            </td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="form-group">
+                                    {/* <input type="text" className="form-control" value={this.state.start_date} onChange={this.onChangeStartDate}></input> */}
+                                    <Calendar value={this.state.start_date} onChange={this.onChangeStartDate}></Calendar>
+                                </div>
+                            </td>
+
+                            <td>
+                                <div className="form-group">
+                                    {/* <input type="text" className="form-control" value={this.state.end_date} onChange={this.onChangeEndDate}></input> */}
+                                    <Calendar value={this.state.end_date} onChange={this.onChangeEndDate}></Calendar>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div className="form-group">
+                                <input type="submit" value="Create Inbound Report" className="btn btn-primary" onClick={this.onSubmitInbound}></input>
+                                </div>
+                            </td>
+
+                            <td>
+                                <div className="form-group">
+                                <input type="submit" value="Create Outbound Report" className="btn btn-primary" onClick={this.onSubmitOutbound}></input>
+                                </div>
+                            </td>
+
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
