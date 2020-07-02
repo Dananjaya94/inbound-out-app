7d8c3026dfccba2a026a21b615d991d3f54f329a
chamikeevunugalla <67573247+chamikeevunugalla@users.noreply.github.com>
2020-07-02 09:02:29 +0530

    Update debug.log

--- src/components/outbound.component.js
+++ src/components/outbound.component.js
@@ -1,63 +1,310 @@
 import React , {Component} from 'react';
 import { render } from '@testing-library/react';
+//import DatePicker from 'react-datepicker';
+//import 'react-datepicker/dist/react-datepicker.css';
 
-export default class outbound extends Component{
-
+export default class reportscomp extends Component{
     constructor(props) {
-        super(props)
-        this.state={
-            provice:'',
-            branch:'',
-            sender_ID:'',
-            branch_code:'',
-            tracking_no:'',
-            EPF_number:'',
-            outbound_date:''
-        }
+        super(props);
+        this.onchange_outbound_province = this.onchange_outbound_province.bind(this);
+        this.onchange_outbound_branch = this.onchange_outbound_branch.bind(this);
+        this.onchange_outbound_sender_id = this.onchange_outbound_sender_id.bind(this);
+        this.onchange_outbound_branch_code = this.onchange_outbound_branch_code.bind(this);
+        this.onchange_outbound_tracking_no = this.onchange_outbound_tracking_no.bind(this);
+        this.onchange_outbound_epf_no = this.onchange_outbound_epf_no.bind(this);
+        this.onchange_outbound_epf_no = this.onchange_outbound_epf_no.bind(this);
+        this.onchange_outbound_outbounddate = this.onchange_outbound_outbounddate.bind(this);
+
+
+    this.state = {
+        outbound_provice: '',
+        outbound_branch: '',
+        outbound_sender_id: '',
+        outbound_branch_code: '',
+        outbound_tracking_no: '',
+        outbound_epf_no: '',
+        outbound_outbounddate: '',
+       // topic:'react',
+       // date: new Date(),
+        
     }
-
-   handleProviceChange = (event)=>{
-       this.setState({
-        provice: event.target.value
-       })
-   }
-   handleBranchChange = (event)=>{
-    this.setState({
-        branch: event.target.value
-    })
-   }
-   handlesenderIdChange = (event)=>{
-    this.setState({
-        sender_ID: event.target.value
-    })
 }
+onchange_outbound_province(e)
+{
+    this.setState({
+        outbound_provice: e.target.value
+    });
+}
+onchange_outbound_branch(e)
+{
+    this.setState({
+        outbound_branch: e.target.value
+    });
+}
+onchange_outbound_sender_id(e)
+{
+    this.setState({
+        outbound_sender_id: e.target.value
+    });
+}
+onchange_outbound_branch_code(e)
+{
+    this.setState({
+        outbound_branch_code: e.target.value
+    });
+}
+onchange_outbound_tracking_no(e)
+{
+    this.setState({
+        outbound_tracking_no: e.target.value
+    });
+}
+onchange_outbound_epf_no(e)
+{
+    this.setState({
+        outbound_epf_no: e.target.value
+    });
+}
+onchange_outbound_outbounddate(e)
+{
+    this.setState({
+        outbound_outbounddate: e.target.value
+    });
+}
+// handleTopicChange(e)
+// {
+//     this.state({
+//         topic: e.target.value
+
+//     });
+// }
+//onChange = date =>this.setState({date})
+
+onSubmit(e){
+        
+    e.preventDefault();
+        console.log(`Form Submitted :`);
+        console.log(`Outbound Province : ${this.state.outbound_provice}`);
+        console.log(`Outbound Branch : ${this.state.outbound_branch}`);
+        console.log(`Outbound Sender ID : ${this.state.outbound_sender_id}`);
+        console.log(`Outbound Sender ID : ${this.state.outbound_branch_code}`);
+        console.log(`Outbound Sender ID : ${this.state.outbound_tracking_no}`);
+        console.log('Outbound EPF no : ${this.state.outbound_epf_no}');
 
 
+
+
+
+        this.setState ({
+        outbound_provice: '',
+        outbound_branch:'',
+        outbound_sender_id: '',
+        outbound_branch_code: '',
+        outbound_tracking_no: '',
+        outbound_epf_no: '',
+        outbound_outbounddate: '',
+       // topic:''
+        })
+
+}
     render()
     {
-        return(
-           
-            <div>
-                <form>
-                    <div>
-                        <label>Provice</label>
-                        <input type='text' value={this.state.provice} onchange={this.handleProviceChange}/>
-                    </div>
-                    <div>
-                        <label>branch</label>
-                        <input type='text' value={this.state.branch} onchange={this.handleBranchChange}/>
-                    </div>
-                    <div>
-                        <label>sender_id</label>
-                        <input type='text' value={this.state.sender_ID} onchange={this.handlesenderIdChange}/>
-                    </div>
-                    
-                </form>
-                
-            </div>
+       // const [selectedDate, setSelectedDate] = useState(null);
 
+        return(
+            <div style={{margin:10}}>
+            <br></br>
+            <br></br>
+            <h2>Outbound Interface</h2>
+            <div className="split left">
+             <div className="centered"></div>
+            <form>
+
+                <table cellPadding="5">
+
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                            <label>Province :</label>
+                            
+                            </div>
+                        </td>
+
+                        <td>
+                            <div className="form-group">
+                            <b><select value={this.state.topic} onchange={this.handleTopicChange}>
+                                <option value="select">select</option>
+                                <option value="western">WESTERN</option>
+                                <option value="southern">SOUTHERN</option>
+                                <option value="nothern">NOTHERN</option>
+                                <option value="uva">UVA</option>
+                                <option value="central">CENTRAL</option>
+                            </select>
+                            </b>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                            <label>Branch :</label>
+                            </div>
+                        </td>
+
+                        <td>
+                            <div className="form-group">
+                            <b><select value={this.state.topic} onchange={this.handleTopicChange}>
+                                <option value="select">select</option>
+                                <option value="colombo">WESTERN</option>
+                                <option value="hambanthota">SOUTHERN</option>
+                                <option value="jaffna">NOTHERN</option>
+                                <option value="badullla">UVA</option><option value="kandy
+                                ">CENTRAL</option>
+                            </select>
+                            </b>
+                                                        </div>
+                        </td>
+                    </tr>
+                    
+
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                            <label>Sender ID :</label>
+                            </div>
+                        </td>
+
+                        <td>
+                            <div className="form-group">
+                            <input type="text" className="form-control" value={this.state.outbound_sender_id} onChange={this.onchange_outbound_sender_id}></input>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                            <label>Branch Code:</label>
+                            </div>
+                        </td>
+
+                        <td>
+                            <div className="form-group">
+                            <input type="text" className="form-control" value={this.state.outbound_branch_code} onChange={this.onchange_outbound_branch_code}></input>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                                <label>Tracking Number :</label>
+                            </div>
+                        </td>
+
+                        <td>
+                            <div className="form-group">
+                                <input type="text" className="form-control" value={this.state.outbound_tracking_no} onChange={this.onchange_outbound_tracking_no}></input>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                                <label>EPF no :</label>
+                            </div>
+                        </td>
+
+                        <td>
+                            <div className="form-group">
+                                <input type="text" className="form-control" value={this.state.outbound_epf_no} onChange={this.onchange_outbound_epf_no}></input>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <div className="form-group">
+                                <label>Outbound date :</label>
+                            </div>
+                        </td>
+
+                        <td>
+                           <div className="form-group">
+                               <input type="text" className="form-control" value={this.state.asset_reciever_epf} onChange={this.onchange_asset_reciever_epf}></input> 
+
+                            </div>
+                        </td>
+
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <tr></tr>
+                        <tr></tr>
+                        <td></td>
+
+                        <td>
+
+                        </td>
+                            
+                        
+                        <td>
+                            <input type="submit" value="Add Outbound" className="btn btn-primary" onClick={this.onSubmit}></input>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                </form>
+                </div>
+                </div>
+                            // <div className="split right">
+
+                            //     <table cellPadding="5">
+
+                            //     <thead>
+                            //     <tr>
+                            //         <th></th>
+                            //         <th></th>
+                            //     </tr>
+                            //     </thead>
+                            //     <tbody>
+                            //     <tr>
+                            //         <td>
+                            //             <div className="form-group">
+                            //             <label>Province :</label>
+                                        
+                            //             </div>
+                            //         </td>
+            
+                            //         <td>
+                            //             <div className="form-group">
+                            //             <b><select value={this.state.topic} onchange={this.handleTopicChange}>
+                            //                 <option value="select">select</option>
+                            //                 <option value="western">WESTERN</option>
+                            //                 <option value="southern">SOUTHERN</option>
+                            //                 <option value="nothern">NOTHERN</option>
+                            //                 <option value="uva">UVA</option>
+                            //                 <option value="central">CENTRAL</option>
+                            //             </select>
+                            //             </b>
+                            //             </div>
+                            //         </td>
+                            //     </tr>
+                            //     </tbody>
+                            //     </table>
+                            //     </div>
+            
                 
-    
-             )
+              
+                
+
+        )
     }
 }
