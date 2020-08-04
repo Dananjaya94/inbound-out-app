60471c9077d0d97f8ae4b574940370a3d71b94e1
Dananjaya Polwattage <nuwan_windows7@live.com>
2020-08-04 16:52:51 +0530

    Update signin.js
    Signin interface completed

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,3 +1,164 @@
+// import React from 'react';
+// import Avatar from '@material-ui/core/Avatar';
+// import Button from '@material-ui/core/Button';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+// import Grid from '@material-ui/core/Grid';
+// import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import Typography from '@material-ui/core/Typography';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Container from '@material-ui/core/Container';
+
+// import inbound from './inbound.component';
+// import {BrowserRouter as Router,Route} from "react-router-dom";
+// import axios from 'axios';
+
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       {'Copyright © '}
+//       <Link color="inherit" href="http://www.ceylinco-insurance.com/" target = "_blank">
+//         Ceylinco General Insurance
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+// var username = '';
+// var password = '';
+
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main,
+//   },
+//   form: {
+//     width: '100%', // Fix IE 11 issue.
+//     marginTop: theme.spacing(1),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//   },
+// }));
+
+// export default function SignIn() {
+  
+//   const classes = useStyles();
+
+//   const routeChange = () =>{
+//     this.history.pushState(null, '/inbound');
+//   }
+
+//   const handleChnageUserName = (e) => {
+//     username = e.target.value
+//   }
+
+//   const handleChangePassword = (e) => {
+//     password = e.target.value
+//   }
+
+//   const handleSubmit = (e) => {
+//     let loginData = {
+//       email : username,
+//       password : password
+//     }
+//     axios.post("http://localhost:4000/uservalidation", loginData,{withCredentials: true})
+//     .then(response => {console.log("Res from login : " , response)})
+//     .catch(error => {console.log("Error : ",error)});
+//   }
+
+
+//   return (
+    
+//     <Container component="main" maxWidth="xs">
+//       <CssBaseline />
+//       <div className={classes.paper}>
+//         <Avatar className={classes.avatar}>
+//           <LockOutlinedIcon />
+//         </Avatar>
+//         <Typography component="h1" variant="h5">
+//           Sign in
+//         </Typography>
+//         <form className={classes.form} noValidate>
+//           <TextField
+//             variant="outlined"
+//             margin="normal"
+//             required
+//             fullWidth
+//             id="email"
+//             label="Email Address"
+//             name="email"
+//             autoComplete="email"
+//             value = {username}
+//             onChange = {handleChnageUserName}
+//             autoFocus
+//           />
+//           <TextField
+//             variant="outlined"
+//             margin="normal"
+//             required
+//             fullWidth
+//             name="password"
+//             label="Password"
+//             type="password"
+//             id="password"
+//             value = {password}
+//             onChange = {handleChangePassword}
+//             autoComplete="current-password"
+//           />
+//           <FormControlLabel
+//             control={<Checkbox value="remember" color="primary" />}
+//             label="Remember me"
+//           />
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             className={classes.submit}
+//             onClick = {handleSubmit}
+//           >
+//             Sign In
+//           </Button>
+//           <Grid container>
+//             <Grid item xs>
+//               <Link href="#" variant="body2">
+//                 Forgot password?
+//               </Link>
+//             </Grid>
+//             <Grid item>
+//               <Link href="#" variant="body2">
+//                 {"Don't have an account? Sign Up"}
+//               </Link>
+//             </Grid>
+//           </Grid>
+//         </form>
+//       </div>
+//       <Box mt={8}>
+//         <Copyright />
+//       </Box>
+
+//       <Router>
+//         <Route path="/inbound" component={inbound}/>
+//         </Router>
+
+
+//     </Container>
+//   );
+// }
+
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -13,15 +174,14 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-import inbound from './inbound.component';
-import {BrowserRouter as Router,Route} from "react-router-dom";
+import axios from 'axios';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="http://www.ceylinco-insurance.com/" target = "_blank">
-        Ceylinco General Insurance
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -49,16 +209,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+var username = '';
+var password = '';
+
 export default function SignIn() {
   const classes = useStyles();
 
-  const routeChange = () =>{
-    this.history.pushState(null, '/inbound');
-  }
-
-
-  return (
+  const handleChnageUserName = (e) => {
+        username = e.target.value
+      }
     
+      const handleChangePassword = (e) => {
+        password = e.target.value
+      }
+
+      const handleSubmit = (e) => {
+        alert(username);
+            let loginData = {
+              email : username,
+              password : password
+            }
+            axios.post("http://localhost:4000/uservalidation", loginData,{withCredentials: true})
+            .then(response => {alert("Res from login : " , response)})
+            .catch(error => {console.log("Error : ",error)});
+          }
+  return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
@@ -70,6 +245,7 @@ export default function SignIn() {
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
+          onChange = {handleChnageUserName}
             variant="outlined"
             margin="normal"
             required
@@ -82,6 +258,7 @@ export default function SignIn() {
           />
           <TextField
             variant="outlined"
+            onChange = {handleChangePassword}
             margin="normal"
             required
             fullWidth
@@ -96,12 +273,12 @@ export default function SignIn() {
             label="Remember me"
           />
           <Button
+          onClick = {handleSubmit}
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
-            onClick = {routeChange}
           >
             Sign In
           </Button>
@@ -122,12 +299,6 @@ export default function SignIn() {
       <Box mt={8}>
         <Copyright />
       </Box>
-
-      <Router>
-        <Route path="/inbound" component={inbound}/>
-        </Router>
-
-
     </Container>
   );
 }
